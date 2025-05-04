11d9bcec6f4e94a85b4173d703e3a8af265a68de
Charles Lima <charles.lima.filho17@gmail.com>
2025-05-04 15:43:48 -0300

    ## Added search for person and print file

--- rust_file/src/main.rs
+++ rust_file/src/main.rs
@@ -1,14 +1,58 @@
+use std::io::prelude::*;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::{self, Write}; // Write has write_all()
 use std::io::stdin;
 use std::io::stdout;
+use std::io::BufReader;
 
 fn main() -> io::Result<()> {
+    let mut option: &str;
     let mut buffer: String = String::new();
-    let mut file: File = OpenOptions::new().write(true).create(true).append(true).open("output.txt")?;
-    //let name: String = "Jorge da Silva".to_string();
-    //let cpf: String = "024.741.852-88".to_string();
+    let caminho: String = "output.txt".to_string();
+    loop {
+        buffer.clear();
+        println!("Escolha uma opção:");
+        println!("1. Adicionar pessoa");
+        println!("2. Buscar pessoa");
+        println!("3. Ler arquivo");
+        println!("0. Sair");
+        stdout().flush()?;
+        stdin().read_line(&mut buffer)?;
+        option = buffer.trim();
+
+        match option {
+            "1" => {
+                if let Err(e) = add_person() {
+                    eprintln!("Error writing to file: {}", e);
+                }
+            }
+            "0" => {
+                println!("Saindo...");
+                break;
+            }
+            "3" => {
+                print_file(caminho.to_string())?;
+            }
+            "2" => {
+                buffer.clear();
+                println!("Digite o CPF: ");
+                stdout().flush()?;
+                stdin().read_line(&mut buffer)?;
+                let cpf: String = buffer.trim().to_string();
+                find_person(caminho.to_string(), cpf)?;
+            }
+            _ => {
+                println!("Opção inválida, tente novamente.");
+            }
+        }
+    }
+    Ok(())
+}
+
+fn add_person() -> io::Result<()> {
+    let mut buffer: String = String::new();
+    let mut file: File = OpenOptions::new().create(true).append(true).open("output.txt")?;
 
     println!("Digite o nome: ");
     stdout().flush()?;
@@ -23,11 +67,58 @@ fn main() -> io::Result<()> {
     buffer.clear();
 
 
-    let message: String = format!("nome: {{ {} }},\nCPF: {{ {} }}\n", name, cpf);
+    let message: String = format!("{{\n\tCPF: {{ {} }},\n\tnome: {{ {} }}\n}}\n", cpf, name);
 
 
     file.write_all(message.as_bytes())?;
     println!("Message written to file.");
+    Ok(())
+}
 
+fn find_person(caminho: String, cpf: String) -> io::Result<()> {
+    let file: File = OpenOptions::new().read(true).open(caminho)?;
+    let reader = BufReader::new(file);
+    let mut cond: bool = false;
+
+    for line in reader.lines() {
+        match line {
+            Ok(_l) => {
+                let l = _l.trim();
+                if l.contains(cpf.as_str()) || cond {
+                    if l == "}" {
+                        println!("");
+                        return Ok(());
+                    }
+
+                    if !cond { println!("") };
+                    println!("{}", l);
+                    cond = true;
+                }
+            } 
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                return Err(e);
+            }
+        }
+    }
+    println!("Não encontrado");
+    return Ok(());
+}
+
+fn print_file(caminho: String) -> io::Result<()> {
+    let file: File = OpenOptions::new().read(true).open(caminho)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line {
+            Ok(_l) => {
+                let l = _l.trim();
+                println!("{}", l);
+            } 
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
     Ok(())
 }
